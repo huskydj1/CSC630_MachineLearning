bde49ade716cce43543d1d39af464d90e4f1469f
Davin Jeong <husky.davin@gmail.com>
2021-10-23 23:09:55 -0400

    Confirm previous commit

--- logisticregression.py
+++ logisticregression.py
@@ -1,79 +1,90 @@
 from variable import Variable
 import numpy as np
 import math
-
-# TODO: SEPARATE VARIABLE PARAMETERS (INITALIZE Y_HAT ONCE) WITH VALUE OF PARAMETERS
-
+import random
 
 class LogisticRegression():
+
+    @staticmethod
+    def dictionify(m_var, b_var, m, b):
+        dict = {}
+        for i in range(len(m_var)):
+            dict[m_var[i].name] = m[i]
+        dict[b_var.name] = b
+        return dict
+
+    @staticmethod
+    def evaluate_model(X, y, m, b):
+        num_samples, num_features = X.shape
+        assert(len(y)==num_samples)
+
+        scores = []
+        for i in range(num_samples):
+            y_hat = 1/(1 + math.exp(-1 * (
+                sum([X[i, j] * m[j] for j in range(num_features)], b)
+            )))
+            y_prediction = 1 if y_hat >= 0.5 else 0
+
+            scores.append(y[i]==y_prediction)
+        
+        return sum(scores)/len(scores)
 
     def fit(self, X, y):
         # Store shape
         self.num_samples, self.num_features = X.shape
         assert(len(y)==self.num_samples)
 
-        # Store parameters
-        self.m_values, self.b_value = self.initializeParameters()
+        # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        # INITALIZE MODEL
 
-        # Gradient Descent
-        for iter in range(0, 100):
-            pass
+        # Create m and b Variable Instances
+        m_var = [Variable(name = f"m_var_{i}") for i in range(self.num_features)]
+        b_var = Variable(name = "zzz_b")
 
-    def generate_costvariable(self, x):
-        
-        # Get Variable cost
-        cost = 0
+        # Create y_hat Variable Instance
+        y_hat_var = []
         for i in range(self.num_samples):
-            y_hat = self.generate_yhat(X[i].flatten())
-            cost += 
-        cost *= -1
+            y_hat_var_i = 1/(1 + Variable.exp(-1 * (
+                sum([m_var[j] * X[i, j] for j in range(self.num_features)], b_var)
+            )))
+            y_hat_var.append(y_hat_var_i)
 
-        # Dictinoify Parameters
-        parameters = {f"m{i}" : self.m_values[i] for i in range(self.num_features)}
-        parameters["b"] = self.b_value
-
-        # Evaluate
-        return y_hat(**parameters)
-    
-    def initializeParameters(self): # TODO: COMPLETELY RANDOMIFY EVERYTHING BY ADDING SEED TO RANDOM
-        m_values = [math.random() - 0.5 for i in range(self.num_features)]
-        b_value = math.random() - 0.5
-
-        return m_values, b_value
+        # Create Cost Variable Instance
+        cost = -1 * sum([(
+            y[i] * Variable.log(y_hat_var[i]) + (1 - y[i]) * Variable.log(1 - y_hat_var[i])
+        ) for i in range(self.num_samples)])
+        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
-    def generate_yhat(self, x):
-        y_hat = sum([Variable(name = f"m{i}") * x[i] for i in range(self.num_features)]) + Variable(name = "b")
-        return y_hat
+        # Generate Random Values of m and b
+        m = [random.random() - 0.50 for i in range(self.num_features)]
+        b = random.random() - 0.50
 
-    def evaluate_cost(self, X):
+        # Initialize Learning Rate
+        learning_rate = 1
 
-        # Get Variable cost
-        cost = 0
-        for i in range(self.num_samples):
-            y_hat = self.generate_yhat(X[i].flatten())
-            cost += 
-        cost *= -1
+        # Training Loop
+        for iter in range(20):
+            parameters = LogisticRegression.dictionify(
+                m_var = m_var, 
+                b_var = b_var, 
+                m = m, 
+                b = b
+            )
 
-        # Dictinoify Parameters
-        parameters = {f"m{i}" : self.m_values[i] for i in range(self.num_features)}
-        parameters["b"] = self.b_value
+            # Get Cost
+            cost_value = cost(**parameters)
+            print(cost_value)
 
-        # Evaluate
-        return y_hat(**parameters)
+            # Get Score on Training Dataset
+            training_score = LogisticRegression.evaluate_model(X, y, m, b)
+            print(training_score)
 
-    def evaluate_cost(self, x):
+            # Take Gradient
+            cost_grad = cost.gradient(**parameters)
+
+            # Update Parameters
+            b -= learning_rate * cost_grad[-1]
+            for i in range(self.num_features):
+                m[i] -= learning_rate * cost_grad[i]
         
-
-    def gradient(self, x):
-        # Get Variable y_hat
-        y_hat = self.generate_yhat(x)
-
-        # Dictinoify Parameters
-        parameters = {f"m{i}" : self.m_values[i] for i in range(self.num_features)}
-        parameters["b"] = self.b_value
-
-        # Take gradient
-        grad_vector = y_hat.grad(**parameters)
-
-        return grad_vector
--- testvariable.py
+++ testvariable.py
@@ -1,9 +1,9 @@
 from variable import Variable
-
+'''
 x_1 = Variable(name = 'x_1')
 x_2 = Variable(name = 'x_2')
 x_3 = Variable(name = 'x_3')
-'''
+
 z = x_1 + x_2 + x_3
 for i in range(100):
     for j in range(100):
@@ -11,23 +11,29 @@ for i in range(100):
             assert(i+j+k == z(x_1 = i, x_2 = j, x_3 = k))
 
 print(z.gradient(x_1 = 1, x_2 = 3, x3 = 5))
-'''
+
 
 z = Variable.exp(x_1 + x_2**2) + 3 * Variable.log(27 - x_1 * x_2 * x_3)
 print(z(x_1 = 3, x_2 = 1, x_3 = 7))
 print(z.gradient(x_1 = 3, x_2 = 1, x_3 = 7))
-
 '''
-class temporaryclass:
-    def __init__(self, value):
-        self.value = value
-    def __add__(self, other):
-        #print(type(self), type(other))
-        return 0
-    def __radd__(self, other):
-        return 0
-
-temporarylist = [temporaryclass(i) for i in range(0, 10)]
-
-print(sum(temporarylist))
 '''
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# PROVES THAT THE SAME INSTANCE OF VARIABLE CAN BE USED MULTIPLE TIMES
+x_1 = Variable(name = 'x_1')
+y_1 = Variable(name = 'y_1')
+cost = x_1 * y_1 + x_1/y_1
+
+print(cost(x_1 = 121, y_1 = 34))
+print(cost.gradient(x_1 = 121, y_1 = 34))
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+'''
+
+x_1 = Variable(name = 'x_1')
+y_1 = Variable(name = 'y_1')
+cost_1 = x_1/y_1
+cost_final = Variable.exp(cost_1) + cost_1
+
+print(cost_final(x_1 = 143, y_1 = 150))
+
+print(cost_final.gradient(x_1 = 143, y_1 = 150))
