4c3e123911bfd6b78e060299ed7b0d12234d5710
huskydj1 <husky.davin@gmail.com>
2021-11-04 10:25:02 -0400

    Try Alpha-Beta Pruning

---
+++ Chess_Bot/cmake-build-debug/CMakeFiles/Chess_Bot.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chess_Bot.dir/demo.cpp.obj"
+  "CMakeFiles/Chess_Bot.dir/thc.cpp.obj"
+  "Chess_Bot.exe"
+  "Chess_Bot.exe.manifest"
+  "Chess_Bot.pdb"
+  "libChess_Bot.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chess_Bot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
